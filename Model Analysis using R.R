5f21932cdd2896e8b108de0d892db000be0afe46
Teresa Le <teresale8@gmail.com>
2023-02-06 19:30:20 -0500

    Update code

--- Model Analysis using R.R
+++ Model Analysis using R.R
@@ -1,17 +1,18 @@
 # Import the data 
-data2 <- read.csv("C:/Users/Teresa - School/Documents/1. U of T SCS - Data Analytics Bootcamp/Module 21 - Project/regreadyvgsales2.csv")
+data <- read.csv("C:/Users/Teresa - School/Documents/GitHub/project-group-4/Data/regreadyvgsales2.csv")
+data1 <- read.csv("C:/Users/Teresa - School/Documents/GitHub/project-group-4/Data/regreadyvgsales3.csv")
+data2 <- read.csv("C:/Users/Teresa - School/Documents/GitHub/project-group-4/Data/regreadyvgsales4.csv")
+data3 <- read.csv("C:/Users/Teresa - School/Documents/GitHub/project-group-4/Data/regreadyvgsales_TOP10PUB.csv")
 
 # Create a multiple linear regression model to predict global sales
-sales.model <- lm(Global_Sales ~ Platform + Year_of_Release + Genre + Publisher + Critic_Score + User_Score + Rating, data2)
+sales.model <- lm(Global_Sales ~ Platform + Year_of_Release + Genre + Publisher + Critic_Score + User_Score + Rating, data)
 
-# View a summary of the results 
-summary(sales.model)
+sales.model2 <- lm(Global_Sales ~ Years_on_Market + Critic_Score + User_Score + Genre_Action + Genre_Adventure + Genre_Fighting + Genre_Misc + Genre_Platform + Genre_Puzzle + Genre_Racing + Genre_Role.Playing + Genre_Shooter + Genre_Simulation + Genre_Sports + Genre_Strategy + Rating_E + Rating_M + Rating_T + Rating_Other, data1)
 
-# Create a model with only the statistically significant values 
-sales.model2 <- lm(Global_Sales ~ Platform + Critic_Score + User_Score + Rating, data2)
+# Update and change 
+sales.model3 <- lm(Global_Sales ~ Years_on_Market + Critic_Score + User_Score + Genre_Action + Genre_Adventure + Genre_Fighting + Genre_Misc + Genre_Platform + Genre_Puzzle + Genre_Racing + Genre_Role.Playing + Genre_Shooter + Genre_Simulation + Genre_Sports + Genre_Strategy + Rating_E + Rating_M + Rating_T + Rating_Other, data2)
 
-# View a summary of the results 
-summary(sales.model2)
-
+#Update and change
+sales.model4 <- lm(Global_Sales ~ Years_on_Market + Critic_Score + User_Score + Genre_Action + Genre_Adventure + Genre_Fighting + Genre_Misc + Genre_Platform + Genre_Puzzle + Genre_Racing + Genre_Role.Playing + Genre_Shooter + Genre_Simulation + Genre_Sports + Genre_Strategy + Rating_E + Rating_M + Rating_T + Rating_Other, data3)
 
 
